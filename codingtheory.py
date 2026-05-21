95c7d308d9eaa6531ef0a95949ee99c3be2bb1b5
thegreatgabsie <gab.an.bongon28@gmail.com>
2026-05-21 23:19:04 +0800

    add functions

--- codingtheory.py
+++ codingtheory.py
@@ -1,6 +1,6 @@
 import numpy as np
+import itertools
 from constants import H
-
 
 def calculate_syndrome(vector):
     syndrome = np.dot(H, np.array(vector).T) % 2
@@ -9,3 +9,23 @@ def calculate_syndrome(vector):
 
 def get_hamming_distance(v1, v2):
     return sum(b1 != b2 for b1, b2 in zip(v1, v2))
+
+def GetParityCheckMatrix(G):
+    k,n = G.shape
+
+    P=G[:,k:]
+
+    identity_nk = np.eye(n - k, dtype=int)
+    H = np.hstack((P.T, identity_nk))
+
+    return H
+
+def GenerateCodespace(GeneratorMatrix):
+    G = np.array(GeneratorMatrix, dtype=int)
+    k,n = G.shape
+
+    messages = np.array(list(itertools.product([0, 1], repeat=k)))
+
+    codewords = (messages @ G) % 2
+
+    return codewords
--- main.py
+++ main.py
@@ -2,10 +2,82 @@ from game import Game
 
 
 def main():
-    print("=== SYNDROME STRIKE ===")
-    print("Each player starts with:")
-    print("- A RANDOM secret codeword")
-    print("- EXACTLY 1 starting error")
+    print("""
+    ====================================
+            WELCOME TO
+            SYNDROME STRIKE
+    ====================================
+
+    A two-player coding theory game
+    where players repair and sabotage
+    codewords using syndrome decoding.
+
+    --------------- MECHANICS ---------------
+
+    • Each player is given:
+        - A correct codeword
+        - A corrupted codeword with 1 error
+
+    • Players take turns making moves.
+
+    • Players can see:
+        - The parity-check matrix
+        - Their own current codeword
+        - Their own Hamming distance
+        - All player's color status
+
+    • Players CANNOT see the exact
+    Hamming distance of their opponent.
+
+    • Instead, the opponent's condition
+    is shown using a color status system.
+
+    ------------- STATUS COLORS -------------
+
+    GREEN
+    • Opponent has exactly 1 error
+    • Their codeword is very close
+    to the correct codeword
+
+    YELLOW
+    • Opponent is still inside the
+    decoding sphere
+    • Recovery is still possible
+
+    RED
+    • Opponent is outside the
+    decoding sphere
+    • Their codeword is heavily corrupted
+
+    --------------- MOVES -------------------
+
+    1. ATTACK
+    - Flip one bit in the opponent's codeword
+    - Used to sabotage their progress
+
+    2. REPAIR
+    - Flip one bit in your own codeword
+    - Used to fix errors
+
+    3. CHECK SYNDROME
+    - Displays the syndrome of your codeword
+    - Helps identify possible error positions
+
+    ------------- WIN CONDITION -------------
+
+    You win if:
+    • You completely repair your codeword
+
+    OR
+
+    • Your opponent becomes too corrupted
+    to recover before you
+
+
+    ====================================
+                GOOD LUCK!
+    ====================================
+    """)
 
     game = Game()
     game.run()
